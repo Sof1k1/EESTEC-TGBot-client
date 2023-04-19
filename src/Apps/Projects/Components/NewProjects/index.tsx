92339fddbef6d54dbcceee6aad8c9034eb207e27
sofyafedorova <sofa888003@gmail.com>
2023-04-19 20:03:47 +0300

    project

--- src/Apps/Projects/Components/NewProjects/index.tsx
+++ src/Apps/Projects/Components/NewProjects/index.tsx
@@ -3,11 +3,103 @@ import * as SolidJS from 'solid-js'
 import './index.css'
 
 const Component: SolidJS.Component = () => {
+    const [IsActive, SetActive] = SolidJS.createSignal(false)
 
     const Element: SolidJS.JSX.Element = (
         <>
-            <div>
-                vcjcjxutx
+            <div class={'project'}>
+                    <button
+                        class={`toggle`}
+                        onClick={(_) => {
+                            SetActive(!IsActive())
+                        }}
+                        no-select={`true`}
+                    >
+                        <span>
+                          Создать Новый проект
+                        </span>
+                    </button>
+                <div class={'preview'}>
+                    <div class={'head'}>
+                        <div class={'title'}>
+                            <span>
+                                Проектная деятельность
+                            </span>
+                        </div>
+                    </div>
+                    <div class={'mainuser'}>
+                        <div class={'name'}>
+                            <span>
+                                Федорова Софья Константиновна
+                            </span>
+                        </div>
+                    </div>
+                    <button
+                        class={`toggle`}
+                        onClick={(_) => {
+                            SetActive(!IsActive())
+                        }}
+                        no-select={`true`}
+                    >
+                        <span>
+                          Просмотр заявок
+                        </span>
+                    </button>
+                </div>
+                <div class={'preview'}>
+                    <div class={'head'}>
+                        <div class={'title'}>
+                            <span>
+                                Проектная деятельность
+                            </span>
+                        </div>
+                    </div>
+                    <div class={'mainuser'}>
+                        <div class={'name'}>
+                            <span>
+                                Федорова Софья Константиновна
+                            </span>
+                        </div>
+                    </div>
+                    <button
+                        class={`toggle`}
+                        onClick={(_) => {
+                            SetActive(!IsActive())
+                        }}
+                        no-select={`true`}
+                    >
+                        <span>
+                          Просмотр заявок
+                        </span>
+                    </button>
+                </div>
+                <div class={'preview'}>
+                    <div class={'head'}>
+                        <div class={'title'}>
+                            <span>
+                                Проектная деятельность
+                            </span>
+                        </div>
+                    </div>
+                    <div class={'mainuser'}>
+                        <div class={'name'}>
+                            <span>
+                                Федорова Софья Константиновна
+                            </span>
+                        </div>
+                    </div>
+                    <button
+                        class={`toggle`}
+                        onClick={(_) => {
+                            SetActive(!IsActive())
+                        }}
+                        no-select={`true`}
+                    >
+                        <span>
+                          Просмотр заявок
+                        </span>
+                    </button>
+                </div>
             </div>
         </>
     )
